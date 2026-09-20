60451e263d7c495d1304af1c765954c37c26d8e7
Amirthaganesh-S <amirthaganesh7521@gmail.com>
2026-09-20 10:23:37 +0530

    Update telegram.py

--- agent /telegram.py
+++ agent /telegram.py
@@ -1,1 +1,22 @@
+import os
+import requests
 
+
+BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
+CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
+
+
+def send_telegram_message(message):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": message,
+        "disable_web_page_preview": False
+    }
+
+    response = requests.post(url, json=payload, timeout=30)
+
+    response.raise_for_status()
+
+    return response.json()
